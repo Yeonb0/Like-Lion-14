17023b51c4d1354da96de2c1efabced1630a1f04
Boyeon Kim <kby7576@sogang.ac.kr>
2026-05-13 20:40:24 +0900

    feat: 0513 세션 - stopwatch 구현

--- likelion-stopwatch/src/App.jsx
+++ likelion-stopwatch/src/App.jsx
@@ -1,9 +1,44 @@
-function App() {
-  return (
-    <div>
+import Timer from "./components/Timer";
+import Record from "./components/Record";
+import { useState } from "react";
+import styled from "styled-components";
 
-    </div>
+const getFormattedDate = () => {
+  const date = ['월', '화', '수', '목', '금', '토', '일'];
+  const now = new Date();
+
+  return `${now.getFullYear()}년 ${now.getMonth() + 1}월 ${now.getDate()}일 ${date[now.getDay() - 1]}요일`;
+}
+
+function App() {
+  const [records, setRecords] = useState([]);
+
+  const handleRecords = (count) => {
+    setRecords((prev) => [...prev, count]);
+  }
+  
+  return (
+    <AppContainer>
+      <h3>🦁오늘은 {getFormattedDate()} 입니다🦁</h3>
+      <Timer handleRecords = {handleRecords} />
+      <Record records = {records} />
+    </AppContainer>
   )
 }
 
 export default App
+
+const AppContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  gap: 120px;
+  padding: 90px;
+  background-color: #010101;
+  min-height: 100vh;
+  color: white;
+  & h3 {
+    font-size: 24px;
+  }
+`
--- likelion-stopwatch/src/components/CircleBox.jsx
+++ likelion-stopwatch/src/components/CircleBox.jsx
@@ -1,7 +1,60 @@
-export function CircleBox() {
+import { useState, useRef, useEffect } from "react"
+import styled from "styled-components";
+
+export function CircleBox({ isPaused = true }) {
+  const [degree, setDegree] = useState(0);
+  const time = useRef(0);
+  const intervalRef = useRef(null);
+
+  useEffect(() => {
+    if (!isPaused) {
+      intervalRef.current = setInterval(() => {
+        setDegree((time.current / 3000) * 360)
+        time.current = time.current + 10 === 3000 // 3000이 되면 0으로, 아니면 10 증가
+          ? 0
+          : time.current + 10
+      }, 10)
+    }
+    return () => { clearInterval(intervalRef.current) }  
+  }, [isPaused]);
+
   return (
-    <div>
-    
-    </div>
+    <CircleWrapper degree={degree}>
+      <Circle>
+        <Dot />
+      </Circle>
+    </CircleWrapper>
   )
 }
+
+const CircleWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 0;
+  width: 300px;
+  height: 300px;
+  transform: rotate(${(props) => props.degree}deg);
+  pointer-events: none; 
+`
+
+const Circle = styled.div`
+  width: 100%;
+  height: 100%;
+  border-radius: 50%;
+  background: none;
+  border: 10px solid #3d3d3d;
+  box-sizing: border-box;   // ✅ border가 width 안에 포함되도록
+`
+  
+const Dot = styled.div`
+  width: 10px;
+  height: 10px;
+  border-radius: 50%;
+  background: #ff492d;
+  position: absolute;
+  left: 50%;
+  top: calc(50% - 145px);
+  transform: translate(-50%, -50%);
+`
+
