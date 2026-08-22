d59b01ec719094cc9b445e1f2c971494fec1ae6e
Boyeon Kim <kby7576@sogang.ac.kr>
2026-08-22 22:05:49 +0900

    feat: 밑바닥부터 시작하는 딥러닝 2 Chap5: 순환 신경망 (RNN) feat: Spring 4 Chap 13: CI/CD 도입하기

--- MLStudy2/common/np.py
+++ MLStudy2/common/np.py
@@ -5,7 +5,9 @@ from common.config import GPU
 if GPU:
     import cupy as np  # type: ignore[import-not-found]
     np.cuda.set_allocator(np.cuda.MemoryPool().malloc)
-    np.add.at = np.scatter_add
+    if not hasattr(np.add, 'at'):
+        # 구버전 cupy 대응 (cupy 14부터는 cupy.add.at 이 기본 제공됨)
+        np.add.at = np.scatter_add
 
     print('\033[92m' + '-' * 60 + '\033[0m')
     print(' ' * 23 + '\033[92mGPU Mode (cupy)\033[0m')
--- MLStudy2/common/optimizer.py
+++ MLStudy2/common/optimizer.py
@@ -122,4 +122,4 @@ class Adam:
             self.m[i] += (1 - self.beta1) * (grads[i] - self.m[i])
             self.v[i] += (1 - self.beta2) * (grads[i]**2 - self.v[i])
             
-            params[i] -= lr_t * self.m[i] / (np.sqrt(self.v[i]) + 1e-7)
+            params[i] -= lr_t * self.m[i] / (np.sqrt(self.v[i]) + 1e-7) 
